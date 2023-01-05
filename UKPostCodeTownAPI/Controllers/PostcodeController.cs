cf472245d200e49dc472e70801c614e05f7ea0f0
Ryan Evans <ryanpaul.evans@pertemps.co.uk>
2023-01-05 13:39:21 +0000

    Add project files.

---
+++ UKPostCodeTownAPI/Controllers/PostcodeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UKPostCodeTownAPI.Models;
+
+namespace UKPostCodeTownAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostcodeController : ControllerBase
+    {
+        [HttpGet(Name = "GetPostcode")]
+        public List<PostcodeModel> Get()
+        {
+            List<PostcodeModel> posts = new List<PostcodeModel>();
+            posts.Add(new PostcodeModel
+            {
+                postcode = "CV7 7PT"
+            });
+
+            return posts;
+            
+        }
+    }
+}
